043885242c80c00cbd505d13aea91f78a11dc7fd
Jonathan Rooney <73543764+jrooney1178@users.noreply.github.com>
2022-11-09 13:53:37 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import logging
-import os
-import comp630
-
-with open("jmr1178.moto", "rb") as f:
-    object_name = os.path.basename(f.name)
-    exception_or_True = comp630.to_the_cloud(f.name, "comp630-m1-f22", f"jmr1178/{object_name}")
-    print(f"Upload Successful: {exception_or_True}")
